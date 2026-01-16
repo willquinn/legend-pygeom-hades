1d78745c9275a993588e55a8b1ecca6d5cdc6485
William Quinn <williamquinn@eduroam-int-dhcp-97-155-149.ucl.ac.uk>
2026-01-16 16:35:39 +0000

    Changed to _read_gdml_model in utils for reading preconstructed gdml files - no need to pass round a gdml directory string. Fixed import of fixed_dimensions.py

--- src/pygeomhades/place_volumes.py
+++ src/pygeomhades/place_volumes.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
-import fixed_dimensions as dim
 import numpy as np
-from pyg4ometry import gdml, geant4
+from pyg4ometry import geant4
+
+from . import fixed_dimensions as dim
+from .utils import _read_gdml_model
 
 # TODO: These functions seem very repetitive, maybe there is a way to reduce
 #      but maybe when/if we move away from loading gdml files this will not be true
@@ -37,13 +39,9 @@ def place_vacuum_cavity(reg: geant4.Registry, world_lv: geant4.LogicalVolume) ->
     return cavity_lv
 
 
-def place_detector(
-    reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, gdml_dir: str | None = None
-) -> None:
-    if gdml_dir:
-        # add detector
-        reader_detector = gdml.Reader(f"{gdml_dir}detector.gdml")
-        reg_detector = reader_detector.getRegistry()
+def place_detector(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
+    if from_gdml:
+        reg_detector = _read_gdml_model("detector.gdml")
         detector_lv = reg_detector.getWorldVolume()
         detector_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -61,11 +59,10 @@ def place_detector(
     reg.addVolumeRecursive(detector_pv)
 
 
-def place_wrap(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, gdml_dir: str | None = None) -> None:
+def place_wrap(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
     # add wrap
-    if gdml_dir:
-        reader_wrap = gdml.Reader(f"{gdml_dir}wrap.gdml")
-        reg_wrap = reader_wrap.getRegistry()
+    if from_gdml:
+        reg_wrap = _read_gdml_model("wrap.gdml")
         wrap_lv = reg_wrap.getWorldVolume()
         wrap_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -82,11 +79,9 @@ def place_wrap(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, gdml_dir: 
     reg.addVolumeRecursive(wrap_pv)
 
 
-def place_holder(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, gdml_dir: str | None = None) -> None:
-    if gdml_dir:
-        # add holder
-        reader_holder = gdml.Reader(f"{gdml_dir}holder.gdml")
-        reg_holder = reader_holder.getRegistry()
+def place_holder(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
+    if from_gdml:
+        reg_holder = _read_gdml_model("holder.gdml")
         holder_lv = reg_holder.getWorldVolume()
         holder_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -103,13 +98,9 @@ def place_holder(reg: geant4.Registry, cavity_lv: geant4.LogicalVolume, gdml_dir
     reg.addVolumeRecursive(holder_pv)
 
 
-def place_bottom_plate(
-    reg: geant4.Registry, world_lv: geant4.LogicalVolume, gdml_dir: str | None = None
-) -> None:
-    if gdml_dir:
-        # add bottom plate
-        reader_plate = gdml.Reader(f"{gdml_dir}bottom_plate.gdml")
-        reg_plate = reader_plate.getRegistry()
+def place_bottom_plate(reg: geant4.Registry, world_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
+    if from_gdml:
+        reg_plate = _read_gdml_model("bottom_plate.gdml")
         plate_lv = reg_plate.getWorldVolume()
         plate_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -126,13 +117,9 @@ def place_bottom_plate(
     reg.addVolumeRecursive(plate_pv)
 
 
-def place_lead_castle(
-    reg: geant4.Registry, world_lv: geant4.LogicalVolume, gdml_dir: str | None = None
-) -> None:
-    if gdml_dir:
-        # add lead castle
-        reader_castle = gdml.Reader(f"{gdml_dir}lead_castle_table1.gdml")
-        reg_castle = reader_castle.getRegistry()
+def place_lead_castle(reg: geant4.Registry, world_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
+    if from_gdml:
+        reg_castle = _read_gdml_model("lead_castle_table1.gdml")
         castle_lv = reg_castle.getWorldVolume()
         castle_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -149,11 +136,10 @@ def place_lead_castle(
     reg.addVolumeRecursive(castle_pv)
 
 
-def place_source(reg: geant4.Registry, world_lv: geant4.LogicalVolume, gdml_dir: str | None = None) -> None:
-    if gdml_dir:
+def place_source(reg: geant4.Registry, world_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
+    if from_gdml:
         # TODO: replace this with a generic reader?
-        reader_source = gdml.Reader(f"{gdml_dir}source_encapsulated_ba_HS4.gdml")
-        reg_source = reader_source.getRegistry()
+        reg_source = _read_gdml_model("source_encapsulated_ba_HS4.gdml")
         source_lv = reg_source.getWorldVolume()
         source_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -171,11 +157,10 @@ def place_source(reg: geant4.Registry, world_lv: geant4.LogicalVolume, gdml_dir:
 
 
 def place_source_holder(
-    reg: geant4.Registry, world_lv: geant4.LogicalVolume, gdml_dir: str | None = None
+    reg: geant4.Registry, world_lv: geant4.LogicalVolume, from_gdml: bool = False
 ) -> None:
-    if gdml_dir:
-        reader_s_holder = gdml.Reader(f"{gdml_dir}plexiglass_source_holder.gdml")
-        reg_s_holder = reader_s_holder.getRegistry()
+    if from_gdml:
+        reg_s_holder = _read_gdml_model("plexiglass_source_holder.gdml")
         s_holder_lv = reg_s_holder.getWorldVolume()
         s_holder_pv = geant4.PhysicalVolume(
             [0, 0, 0],
@@ -192,10 +177,9 @@ def place_source_holder(
     reg.addVolumeRecursive(s_holder_pv)
 
 
-def place_cryostat(reg: geant4.Registry, world_lv: geant4.LogicalVolume, gdml_dir: str | None = None) -> None:
-    if gdml_dir:
-        reader_cryo = gdml.Reader(f"{gdml_dir}cryostat.gdml")
-        reg_cryo = reader_cryo.getRegistry()
+def place_cryostat(reg: geant4.Registry, world_lv: geant4.LogicalVolume, from_gdml: bool = False) -> None:
+    if from_gdml:
+        reg_cryo = _read_gdml_model("cryostat.gdml")
         cryo_lv = reg_cryo.getWorldVolume()
         cryo_pv = geant4.PhysicalVolume(
             [0, 0, 0], [0, 0, 0, "mm"], cryo_lv, "hpge_physical", world_lv, registry=reg_cryo
--- src/pygeomhades/utils.py
+++ src/pygeomhades/utils.py
@@ -1,37 +1,15 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Container
 from importlib import resources
 
 import pyg4ometry
 from pyg4ometry import geant4
 
-from . import core
-
 log = logging.getLogger(__name__)
 
 
-def _read_model(
-    file: str, name: str, material: geant4.Material, b: core.InstrumentationData
-) -> geant4.LogicalVolume | None:
-    """
-    Construct a logical volume for an STL mesh.
-
-    .. note::
-        This function honours the ``no_meshes`` runtime configuration, which can either be ``True``
-        to disable all meshes, or be a list of logical volume names to disable mesh loading.
-
-    Returns
-    -------
-    A :class:`geant4.LogicalVolume` for the mesh or ``None``, if loading of this mesh is disabled.
-    """
-    # this is an (undocumented) option to remove meshes; either all or from a list (for performance tests).
-    no_meshes = b.runtime_config.get("no_meshes", False)
-    if (isinstance(no_meshes, Container) and (name in no_meshes)) or no_meshes is True:
-        log.warning("skipping mesh %s", name)
-        return None
-
+def _read_gdml_model(file: str) -> geant4.Registry:
     file = resources.files("pygeomhades") / "models" / file
-    solid = pyg4ometry.stl.Reader(file, solidname=name, centre=False, registry=b.registry).getSolid()
-    return geant4.LogicalVolume(solid, material, name, b.registry)
+    reader = pyg4ometry.gdml.Reader(file)
+    return reader.getRegistry()
